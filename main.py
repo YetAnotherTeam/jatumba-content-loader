14ac6cb8289c74cda23387e5a84b3c2e4993fd19
b.khasanov <afti@yandex.ru>
2016-08-10 00:57:12 +0300

    Добавил парсер персонажей игры престолов.

--- main.py
+++ main.py
@@ -2,12 +2,54 @@ import random
 from urllib.parse import urljoin
 
 import requests
+from bs4 import BeautifulSoup
 from faker import Faker
 
 from settings import *
 from utils import Gender
 
 fake = Faker()
+
+
+class GOTParser(object):
+    BASE_URL_TEMPLATE = (
+        'http://gameofthrones.wikia.com/index.php?action=ajax&articleId=Characters'
+        '&method=axGetArticlesPage&rs=CategoryExhibitionAjax&page={page}'
+    )
+    partition_part = 'gameofthrones/images'
+
+    def get_pages_count(self):
+        json_response = requests.get(self.BASE_URL_TEMPLATE.format(page=1)).json()
+        soup = BeautifulSoup(json_response['paginator'], 'html.parser')
+        return int(soup.find_all('a', attrs={'class': 'paginator-page'})[-1].string)
+
+    def prepare_image_url(self, url):
+        prepared_url = (
+            'http://vignette2.wikia.nocookie.net/' + self.partition_part +
+            url.partition(self.partition_part)[2]
+        )
+        prepared_url = prepared_url.replace('images/thumb', 'images/').rsplit('/', 1)[0]
+        return prepared_url
+
+    def parse_page(self, page):
+        json_response = requests.get(self.BASE_URL_TEMPLATE.format(page=page)).json()
+        soup = BeautifulSoup(json_response['page'], 'html.parser')
+        characters_imgs = soup.find_all('img')
+        result = [{
+            'character_name': character_img['alt'],
+            'image': self.prepare_image_url(character_img['src'])
+        } for character_img in characters_imgs]
+        return result
+
+    def parse(self):
+        pages_count = self.get_pages_count()
+        result_list = []
+        for page in range(1, pages_count + 1):
+            result_list.extend(self.parse_page(page))
+        for i in result_list:
+            print(i)
+        print(len(result_list))
+        return result_list
 
 
 class DataGenerator(object):
@@ -22,47 +64,39 @@ class DataGenerator(object):
         Gender.F: ('first_name_female', 'last_name_female')
     }
 
+    def __init__(self, parsed_characters):
+        self.parsed_characters = parsed_characters
+
     def _get_split_full_name(self):
         gender = random.choice([Gender.M, Gender.F])
         return [getattr(fake, method_name)() for method_name in self.GENDER_CHOOSE_MAP[gender]]
 
-    def get_fake_avatars(self):
-        response = requests.get(RANDOM_USERS_URL_TEMPLATE.format(results=USERS_COUNT))
-        for json_user in response.json()['results']:
-            self.avatar_urls.append(json_user['picture']['large'])
-
     def generate_access_tokens(self):
-        for i in range(USERS_COUNT):
-            try_number = 0
-            response = None
-            avatar_url = self.avatar_urls[i]
-            while True:
-                first_name, last_name = self._get_split_full_name()
-                avatar_response = requests.get(avatar_url)
-                response = requests.post(
-                    urljoin(URL, SIGN_UP_URL),
-                    data={
-                        'username': fake.user_name(),
-                        'password': fake.password(),
-                        'first_name': first_name,
-                        'last_name': last_name
-                    },
-                    files={
-                        'avatar': (avatar_url.rsplit('/', 1), avatar_response.content)
-                    }
-                )
-                print(response.content)
-                if response.status_code != 201:
-                    try_number += 1
-                    print(
-                        'Try {count}: {error}'.format(
-                            count=try_number,
-                            error=response.json()['username'][0]
-                        )
-                    )
-                else:
-                    break
-            self.access_tokens.append(response.json()['session']['access_token'])
+        for parsed_character in self.parsed_characters:
+            name = parsed_character['character_name']
+            split_full_name = name.split(maxsplit=1)
+            first_name = split_full_name[0]
+            last_name = '' if len(split_full_name) == 1 else split_full_name[1]
+            username = name.replace(' ', '')
+            avatar_url = parsed_character['image']
+            avatar_response = requests.get(avatar_url)
+            response = requests.post(
+                urljoin(URL, SIGN_UP_URL),
+                data={
+                    'username': username,
+                    'password': fake.password(),
+                    'first_name': first_name,
+                    'last_name': last_name
+                },
+                files={
+                    'avatar': (avatar_url.rsplit('/', 1), avatar_response.content)
+                }
+            )
+            print(response.content)
+            if response.status_code != 201:
+                print('{error}'.format(error=response.json()['username'][0]))
+            else:
+                self.access_tokens.append(response.json()['session']['access_token'])
 
     def create_bands(self):
         for i in range(BANDS_COUNT):
@@ -111,7 +145,7 @@ class DataGenerator(object):
             )
 
     def run(self):
-        self.get_fake_avatars()
+        # self.get_fake_avatars()
         self.generate_access_tokens()
         self.create_bands()
         self.create_compositions()
@@ -120,7 +154,8 @@ class DataGenerator(object):
 
 
 def main():
-    DataGenerator().run()
+    parsed_characters = GOTParser().parse()
+    DataGenerator(parsed_characters).run()
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-URL = 'http://p30112.lab1.stud.tech-mail.ru/'
+URL = 'https://jatumba.ml/'
 # URL = 'http://127.0.0.1:8000/'
 SIGN_UP_URL = '/api/user/sign_up/'
 BANDS_URL = '/api/band/'
@@ -6,10 +6,7 @@ COMPOSITIONS_URL = '/api/composition/'
 MEMBER_URL = '/api/member/'
 INSRTUMENT_URL = '/api/instrument/'
 
-RANDOM_USERS_URL_TEMPLATE = 'http://api.randomuser.me/?results={results}'
-
-USERS_COUNT = 40
-BANDS_COUNT = 100
-COMPOSITIONS_COUNT = 100
-MEMBER_COUNT = 800
+BANDS_COUNT = 800
+COMPOSITIONS_COUNT = 800
+MEMBER_COUNT = 2400
 
